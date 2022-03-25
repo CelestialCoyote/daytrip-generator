c41e49ae755f8bbf5d8879926480dcf894177803
Paul Stearns <coyote@Pauls-MBP.routerlogin.net>
2022-03-25 14:14:39 -0400

    Added arrays for random choice options to app.js

--- app.js
+++ app.js
@@ -1,2 +1,42 @@
 'use strict';
 
+
+// Setup arrays for random choices.
+
+let destinationLocations = [
+    'London',
+    'Amsterdam',
+    'Madrid',
+    'Brussels',
+    'Paris',
+    'Berlin',
+    'Rome'
+];
+
+let restaurantChoices = [
+    'Steakhouse',
+    'Italian Cuisine',
+    'French Cuisine',
+    'Chinese Cuisine',
+    'Mexican Cuisine',
+    'Biergarten',
+    'Fast Food',
+    'Pub'
+];
+
+let transportaionModes = [
+    'Walking Tour',
+    'Rental Car',
+    'Airplane',
+    'Bus',
+    'Boat'
+];
+
+let entertainmentChoices = [
+    'Movie',
+    'Opera',
+    'Nightclub',
+    'Ballet',
+    'Gambling'
+];
+
